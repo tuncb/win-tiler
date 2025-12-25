e10144f28380b0a5d91c76ef5ea40918819a16ee
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2025-12-25 18:23:40 +0100

    add claude to default ignore list

--- src/options.cpp
+++ src/options.cpp
@@ -15,7 +15,8 @@ IgnoreOptions get_default_ignore_options() {
       {"SystemSettings.exe", "Settings"},
       {"explorer.exe", "Program Manager"},
       {"explorer.exe", "System tray overflow window."},
-      {"explorer.exe", "PopupHost"}
+      {"explorer.exe", "PopupHost"},
+      {"claude.exe", "Title: Claude"},
   };
   options.small_window_barrier = SmallWindowBarrier{50, 50};
   return options;
@@ -25,16 +26,11 @@ GlobalOptions get_default_global_options() {
   GlobalOptions options;
   options.ignoreOptions = get_default_ignore_options();
   options.keyboardOptions.bindings = {
-      {HotkeyAction::NavigateLeft, "super+shift+h"},
-      {HotkeyAction::NavigateDown, "super+shift+j"},
-      {HotkeyAction::NavigateUp, "super+shift+k"},
-      {HotkeyAction::NavigateRight, "super+shift+l"},
-      {HotkeyAction::ToggleSplit, "super+shift+y"},
-      {HotkeyAction::Exit, "super+shift+escape"},
-      {HotkeyAction::ToggleGlobal, "super+shift+;"},
-      {HotkeyAction::StoreCell, "super+shift+["},
-      {HotkeyAction::ClearStored, "super+shift+]"},
-      {HotkeyAction::Exchange, "super+shift+,"},
+      {HotkeyAction::NavigateLeft, "super+shift+h"}, {HotkeyAction::NavigateDown, "super+shift+j"},
+      {HotkeyAction::NavigateUp, "super+shift+k"},   {HotkeyAction::NavigateRight, "super+shift+l"},
+      {HotkeyAction::ToggleSplit, "super+shift+y"},  {HotkeyAction::Exit, "super+shift+escape"},
+      {HotkeyAction::ToggleGlobal, "super+shift+;"}, {HotkeyAction::StoreCell, "super+shift+["},
+      {HotkeyAction::ClearStored, "super+shift+]"},  {HotkeyAction::Exchange, "super+shift+,"},
       {HotkeyAction::Move, "super+shift+."},
   };
   return options;
