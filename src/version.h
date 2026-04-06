20984e3d21a809da5ae0cc3e19a115890054f098
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2026-04-06 20:55:56 +0200

    Bump version to 0.3.0-alpha
    - Update the version constants
    - Refresh the version string test

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
 namespace wintiler {
 
 constexpr int VERSION_MAJOR = 0;
-constexpr int VERSION_MINOR = 2;
+constexpr int VERSION_MINOR = 3;
 constexpr int VERSION_PATCH = 0;
 constexpr const char* VERSION_LABEL = "alpha";
 
