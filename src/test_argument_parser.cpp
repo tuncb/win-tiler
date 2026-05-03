759aa9a565902a6bb1b09e876684a158e23b88b7
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2026-05-03 15:37:05 +0200

    Bump version to 0.5.0

--- src/test_argument_parser.cpp
+++ src/test_argument_parser.cpp
@@ -165,7 +165,7 @@ TEST_SUITE("argument_parser") {
 
 TEST_SUITE("version") {
   TEST_CASE("version string matches the current release version") {
-    CHECK(get_version_string() == "0.4.8");
+    CHECK(get_version_string() == "0.5.0");
   }
 
   TEST_CASE("version string does not include a prerelease suffix") {
