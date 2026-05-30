5b9748fc5f9263544ccc525fa8f25b4a8ac6fd21
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2026-05-30 16:21:34 +0200

    Bump version to 0.8.1

--- src/version.h
+++ src/version.h
@@ -6,7 +6,7 @@ namespace wintiler {
 
 constexpr int VERSION_MAJOR = 0;
 constexpr int VERSION_MINOR = 8;
-constexpr int VERSION_PATCH = 0;
+constexpr int VERSION_PATCH = 1;
 
 // Build version string from constants (single source of truth)
 inline std::string get_version_string() {
