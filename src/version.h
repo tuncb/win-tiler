b2c432dcf207ced3333ccfd4edfbca12f383fde9
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2026-09-19 13:09:31 +0200

    Bump app version to 0.12.0

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
 namespace wintiler {
 
 constexpr int VERSION_MAJOR = 0;
-constexpr int VERSION_MINOR = 11;
+constexpr int VERSION_MINOR = 12;
 constexpr int VERSION_PATCH = 0;
 
 // Build version string from constants (single source of truth)
