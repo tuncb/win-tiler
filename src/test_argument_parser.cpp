8603a7aa950bac99417348a4aa172795603dc9fb
Tunç Bahçecioğlu <tunc.bahcecioglu@gmail.com>
2026-09-19 14:05:42 +0200

    Bump app version to 0.12.1

--- src/test_argument_parser.cpp
+++ src/test_argument_parser.cpp
@@ -323,7 +323,7 @@ TEST_SUITE("argument_parser") {
 
 TEST_SUITE("version") {
   TEST_CASE("version string matches the current release version") {
-    CHECK(get_version_string() == "0.12.0");
+    CHECK(get_version_string() == "0.12.1");
   }
 
   TEST_CASE("version string does not include a prerelease suffix") {
